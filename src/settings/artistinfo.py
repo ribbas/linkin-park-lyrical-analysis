0268616d2661bbb2d1bf4a6fa95c56f6926efcd5
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2017-08-19 11:17:12 -0400

    Update artistinfo.py

--- src/settings/artistinfo.py
+++ src/settings/artistinfo.py
@@ -18,13 +18,3 @@ LINKIN_PARK_ALBUMS = (
     "the-hunting-party",
     "one-more-light",
 )
-
-ALBUMS_TIMESTAMP = {
-    "hybrid-theory": "2000",
-    "meteora": "2003",
-    "minutes-to-midnight": "2007",
-    "a-thousand-suns": "2010",
-    "living-things": "2012",
-    "the-hunting-party": "2014",
-    "one-more-light": "2017",
-}
